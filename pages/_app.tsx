40f824762a6d01f6ba01a1977976addb14645099
Daryl Stensland <dstensland5@gmail.com>
2022-11-21 22:24:34 +0000

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+
+import { Poppins } from '@next/font/google';
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+const poppins = Poppins({
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+  style: 'normal',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
